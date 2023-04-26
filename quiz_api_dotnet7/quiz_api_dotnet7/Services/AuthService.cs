265691779ab67732a87616a30691f477c1cca250
rod <rodrigovalladares1@gmail.com>
2023-04-26 08:45:57 -0600

    ♻️implement services

---
+++ quiz_api_dotnet7/quiz_api_dotnet7/Services/AuthService.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using NuGet.Common;
+using quiz_api_dotnet7.Data;
+using quiz_api_dotnet7.Interfaces;
+using quiz_api_dotnet7.Models;
+using quiz_api_dotnet7.Models.Auth;
+using quiz_api_dotnet7.Utilities;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace quiz_api_dotnet7.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly QuizContext _context;
+        private readonly IConfiguration _configuration;
+
+        public AuthService(QuizContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        public LoginResponse Login(LoginRequest login)
+        {
+            if (_context.Users == null)
+            {
+                throw new InvalidOperationException(Errors.NotFound);
+            }
+
+            var user = _context.Users
+                .Where(u => u.Email == login.Email && u.Password == login.Password)
+                .FirstOrDefault();
+
+            if (user == null)
+            {
+                return new LoginResponse
+                {
+                    Success = false,
+                    Message = Errors.BadLogin,
+                    Result = ""
+                };
+            }
+
+            var jwt = _configuration.GetSection("Jwt").Get<CustomJwt>();
+
+            if (jwt == null)
+            {
+                return new LoginResponse
+                {
+                    Success = false,
+                    Message = Errors.BadRequest,
+                    Result = ""
+                };
+            }
+
+            var claim = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, jwt.Subject),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Sub, DateTime.UtcNow.ToString()),
+                new Claim("id", user.Id.ToString()),
+                new Claim("role", user.Role.ToString()),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt.Key));
+            var singIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                jwt.Issuer,
+                jwt.Audience,
+                claim,
+                expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: singIn
+                );
+
+            return new LoginResponse
+            {
+                Success = true,
+                Message = Success.SuccessLogin,
+                Result = new JwtSecurityTokenHandler().WriteToken(token)
+            };
+        }
+
+        public RegisterResponse? Register(RegisterRequest register)
+        {
+            var IsUserNameExist = _context.Users.Any(u => u.UserName == register.UserName);
+
+            if (IsUserNameExist) 
+            {
+                return new RegisterResponse
+                {
+                    Success = false,
+                    Message = Errors.IsUserNameExist
+                };
+            }
+
+            var IsEmailExist = _context.Users.Any(u => u.Email == register.Email);
+
+            if (IsEmailExist)
+            {
+                return new RegisterResponse
+                {
+                    Success = false,
+                    Message = Errors.IsEmailExist
+                };
+            }
+
+            var user = new User
+            {
+                FirstName = register.FirstName,
+                LastName = register.LastName,
+                UserName = register.UserName,
+                Email = register.Email,
+                Password = register.Password,
+                Role = UserRoleType.Customer
+            };
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            return new RegisterResponse
+            {
+                Success = true,
+                Message = Success.SuccessRegister,
+            };
+        }
+    }
+}
--- quiz_api_dotnet7/quiz_api_dotnet7/Services/UserService.cs
+++ quiz_api_dotnet7/quiz_api_dotnet7/Services/UserService.cs
@@ -3,6 +3,7 @@ using Microsoft.IdentityModel.Tokens;
 using quiz_api_dotnet7.Data;
 using quiz_api_dotnet7.Interfaces;
 using quiz_api_dotnet7.Models;
+using quiz_api_dotnet7.Models.Auth;
 using quiz_api_dotnet7.Utilities;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -13,88 +14,20 @@ namespace quiz_api_dotnet7.Services
     public class UserService : IUserService
     {
         private readonly QuizContext _context;
-        private readonly IConfiguration _configuration;
 
-        public UserService(QuizContext context, IConfiguration configuration)
+        public UserService(QuizContext context)
         {
             _context = context;
-            _configuration = configuration;
         }
 
-        public LoginResponse Login(LoginRequest login)
+        public IEnumerable<User> GetAll()
         {
-            if (_context.Users == null)
-            {
-                throw new InvalidOperationException(Erros.NotFound);
-            }
-
-            var user = _context.Users
-                .Where(u => u.Email == login.Email && u.Password == login.Password)
-                .FirstOrDefault();
-
-            if (user == null)
-            {
-                return new LoginResponse
-                {
-                    Success = true,
-                    Message = Erros.BadLogin,
-                    Result = ""
-                };
-            }
-
-            var jwt = _configuration.GetSection("Jwt").Get<CustomJwt>();
-
-            if (jwt == null)
-            {
-                return new LoginResponse
-                {
-                    Success = true,
-                    Message = Erros.BadRequest,
-                    Result = ""
-                };
-            }
-
-            var claim = new[]
-            {
-                new Claim(JwtRegisteredClaimNames.Sub, jwt.Subject),
-                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                new Claim(JwtRegisteredClaimNames.Sub, DateTime.UtcNow.ToString()),
-                new Claim("id", user.Id.ToString()),
-                new Claim("role", user.Role.ToString()),
-            };
-
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt.Key));
-            var singIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-
-            var token = new JwtSecurityToken(
-                jwt.Issuer,
-                jwt.Audience,
-                claim,
-                expires: DateTime.Now.AddMinutes(60),
-                signingCredentials: singIn
-                );
-
-            return new LoginResponse
-            {
-                Success = true,
-                Message = "Inicio de session exitoso",
-                Result = new JwtSecurityTokenHandler().WriteToken(token)
-            };
+            return _context.Users.OrderBy(m => m.FirstName).ToList();
         }
 
         public User? GetById(int userId)
         {
-            return _context.Users
-                .AsNoTracking()
-                .SingleOrDefault(u => u.Id == userId);
-        }
-
-        public User? PostUser(User user)
-        {
-            _context.Users.Add(user);
-            _context.SaveChanges();
-
-            return user;
+            return _context.Users.SingleOrDefault(u => u.Id == userId);
         }
     }
 }
