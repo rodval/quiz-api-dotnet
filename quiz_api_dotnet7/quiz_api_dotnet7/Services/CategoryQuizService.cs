21469f5ddd14cdb67c8a6f94b6d7f326774160f7
Rodrigo Valladares <rodrigovalladares1@gmail.com>
2023-05-17 23:53:32 -0600

    implement category quiz level

--- quiz_api_dotnet7/quiz_api_dotnet7/Services/CategoryQuizService.cs
+++ quiz_api_dotnet7/quiz_api_dotnet7/Services/CategoryQuizService.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using quiz_api_dotnet7.Data;
 using quiz_api_dotnet7.Interfaces;
 using quiz_api_dotnet7.Models.Quiz;
+using System.Linq;
 
 namespace quiz_api_dotnet7.Services
 {
@@ -28,14 +30,28 @@ namespace quiz_api_dotnet7.Services
 
         public IEnumerable<CategoryQuiz> GetAllByUser(int userId)
         {
-            return _context.CategoryQuizzes
-                           .Include(cq => cq.Category)
-                           .AsNoTracking()
-                           .Include(cq => cq.UserQuizzes.Where(u => u.UserId == userId).OrderByDescending(u => u.Score).Take(1))
-                           .AsNoTracking()
-                           .OrderBy(cq => cq.Category)
-                           .ThenBy(cq => cq.Level)
-                           .ToList();
+            var categories = _context.Categories.ToList();
+
+            var categoryQuiz = _context.CategoryQuizzes
+                                       .Include(cq => cq.UserQuizzes.Where(u => u.UserId == userId).OrderByDescending(u => u.Score).Take(1))
+                                       .AsNoTracking()
+                                       .ToList();
+
+            var isCategoryQuizWithOutTry = categoryQuiz.Where(cq => cq.UserQuizzes.IsNullOrEmpty());
+
+            var currentCategoryQuizLevel = new List<CategoryQuiz> { };
+
+            foreach (var category in categories)
+            {
+                var levelCategory = isCategoryQuizWithOutTry.Where(cq => cq.CategoryId == category.Id).OrderBy(cq => cq.Level).Take(1);
+
+                if (levelCategory.Any()) 
+                {
+                    currentCategoryQuizLevel.AddRange(levelCategory);
+                }
+            }
+
+            return currentCategoryQuizLevel;
         }
     }
 }
